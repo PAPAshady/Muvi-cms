579ee733752c94ecc0bf2570d57522d498c4302d
Nima Zamani <zamani.nima18@gmail.com>
2024-07-18 02:27:10 +0330

    added deleteSeries function to delete series completely

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ function showAllSeries () {
                     <div class="media-info">
                         <a href="#">${series[1].title}</a>
                         <div class="btn-wrapper">
-                            <button class="btn-fill">
+                            <button onclick="deleteSeries(event, '${series[1].title}', '${series[0]}')" class="btn-fill">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                                 </svg>
@@ -247,6 +247,31 @@ function showAllSeries () {
     }).join('')
 
     allSeriesContainer.insertAdjacentHTML('beforeend', seriesElems)
+}
+
+function deleteSeries(e,seriesTitle, seriesID){
+    e.preventDefault()
+    const isSure = confirm(`Are you sure you want to delete ${seriesTitle} completly ? this action is permanent and it will delete all this series seasons and episodes`)
+
+    if(isSure){
+        const shouldDelete = prompt(`If you are sure about deleting ${seriesTitle} permanently, please write  ${seriesTitle}  in the input below`)
+
+        if(shouldDelete.toUpperCase().trim() === seriesTitle.toUpperCase()){
+            fetch(`https://muvi-86973-default-rtdb.asia-southeast1.firebasedatabase.app/series/${seriesID}.json`, {
+                method : 'DELETE'
+            })
+                .then(res => res.json())
+                .then(message => {
+                    alert(`${seriesTitle} deleted successfully !`)
+                    console.log(message);
+                }).catch(err => {
+                    alert(`An error occured while deleting ${seriesTitle} series`)
+                    console.log(err);
+                })
+        }else{
+            alert("You've entered the wrong name. Series will not be deleted haha !")
+        }
+    }
 }
 
 function clearInputs () {
