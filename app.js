25424bae3441ec1d208e44ee874ce0e36f0e7a53
Nima Zamani <zamani.nima18@gmail.com>
2024-07-18 05:59:59 +0330

    added openModal and closeModal functions

--- app.js
+++ app.js
@@ -18,9 +18,15 @@ const ratingInput = $.getElementById('ratingInput')
 const castsInput = $.getElementById('castsInput')
 const checkBox = $.getElementById('checkBox')
 const searchInput = $.getElementById('searchInput')
+const modalWrapper = $.querySelector('.modal-wrapper')
+const modalTitle = $.getElementById('modalTitle')
+const closeModalBtn = $.getElementById('closeModalBtn')
 const allSeriesContainer = $.querySelector('.media-content-wrapper')
+const editSeriesInfosBtn = $.getElementById('editSeriesInfosBtn')
+const editSeriesEpisodesBtn = $.getElementById('editSeriesEpisodesBtn')
 let genres = []
 let casts = []
+let seriesID = null
 let allSeries = null
 
 
@@ -54,7 +60,7 @@ async function showSeries (seriesArray) {
                                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                                 </svg>
                             </button>
-                            <button class="btn-fill">
+                            <button onclick="openModal(event, '${series[0]}')" class="btn-fill">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                 </svg>
@@ -256,13 +262,13 @@ function addNewSeries (e){
             body : JSON.stringify(newSeries)
         })
             .then(res => res.json())
-            .then(message => {
+            .then(getAllSeries)
+            .then(()=>{
                 alert('Series added successfully :)')
                 clearInputs()
-                showSeries()
+                showSeries(allSeries)
                 window.scrollTo({top : 0, behavior : 'smooth'})
                 $.body.classList.remove('add-series')
-                console.log(message);
             })
             .catch(err => {
                 alert('Error, something went wrong. Please turn on your VPN and try again :)')
@@ -288,17 +294,29 @@ function deleteSeries(e,seriesTitle, seriesID){
                 method : 'DELETE'
             })
                 .then(res => res.json())
-                .then(message => {
+                .then(getAllSeries)
+                .then(()=>{
                     alert(`${seriesTitle} deleted successfully !`)
-                    showSeries()
-                    console.log(message);
-                }).catch(err => {
+                    showSeries(allSeries)
+                })
+                .catch(err => {
                     alert(`An error occured while deleting ${seriesTitle} series`)
                     console.log(err);
                 })
         }else{
             alert("You've entered the wrong name. Series will not be deleted haha !!!")
         }
+    }
+}
+
+function openModal (e, id) {
+    e.preventDefault()
+    $.body.classList.add('show-modal')
+}
+
+function closeModal(e){
+    if(e.target.className === 'modal-wrapper' || closeModalBtn.contains(e.target)){
+        $.body.classList.remove('show-modal')
     }
 }
 
@@ -315,6 +333,7 @@ function clearInputs () {
 submitSeriesBtn.addEventListener('click', addNewSeries)
 imageUrlInput.addEventListener('input', showImagePreviewHandler)
 searchInput.addEventListener('input', searchHandler)
+modalWrapper.addEventListener('click', e => closeModal(e))
 
 tagsInput.addEventListener('keydown', e => {
     if(e.key === 'Enter'){
