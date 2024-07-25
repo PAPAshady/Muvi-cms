d58ccf777c3e656c18b8a6f52d98f119ba32e291
Nima Zamani <zamani.nima18@gmail.com>
2024-07-25 20:54:51 +0330

    added firebase SDK to project

---
+++ packages/Firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp  } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js'
+import { getStorage, ref  } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCr_lREx3D2tFfbCAnSlkq-imQKPlUA_5s",
+    authDomain: "muvi-86973.firebaseapp.com",
+    databaseURL: "https://muvi-86973-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "muvi-86973",
+    storageBucket: "muvi-86973.appspot.com",
+    messagingSenderId: "88955528642",
+    appId: "1:88955528642:web:8cf9c8d4b73fb960e1625b",
+    measurementId: "G-374K5GBKDV"
+};
+
+const app = initializeApp(firebaseConfig)
+const storage = getStorage()
+const storageRef = ref(storage)
