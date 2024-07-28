6f37856d3ebcd2d660083a860b95bda6141e7421
papashady <zamani.nima18@gmail.com>
2024-07-28 21:30:46 +0430

    imported requaired firebase methods for crud operations

--- packages/Firebase.js
+++ packages/Firebase.js
@@ -1,6 +1,17 @@
 import { initializeApp  } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js'
 import { getStorage, ref, uploadBytesResumable } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js'
-import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js'
+import {
+    getFirestore,
+    collection,
+    getDoc,
+    setDoc,
+    addDoc,
+    doc,
+    updateDoc,
+    arrayUnion,
+    arrayRemove,
+    deleteDoc,
+} from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCr_lREx3D2tFfbCAnSlkq-imQKPlUA_5s",
@@ -22,4 +33,11 @@ window.uploadBytesResumable = uploadBytesResumable
 window.storage = storage
 window.db = db
 window.collection = collection
+window.getDoc = getDoc
+window.setDoc = setDoc
 window.addDoc = addDoc
+window.doc = doc
+window.updateDoc = updateDoc
+window.arrayUnion = arrayUnion
+window.arrayRemove = arrayRemove
+window.deleteDoc = deleteDoc
