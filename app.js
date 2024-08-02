98a5bacf4640a9c5901b1fb595623e3b2e0887b6
papashady <zamani.nima18@gmail.com>
2024-08-02 08:12:49 +0430

    added removeEpisode Function and modified removeSeasons function for better re-usabilty

--- app.js
+++ app.js
@@ -502,24 +502,22 @@ async function addEpisodeOrSeason () {
             })
 
             currentSeries.seasons = seasons
-
+            folderRef = `series/${currentSeries.seriesID}/season${seasonNumber}/episode${currentEpisodeNumber}`
         }else{
             currentEpisodeNumber = currentSeries.seasons[seasonNumber - 1].episodes.length
             newEpisode.episodeID = `${currentSeries.seriesID}-S${seasonNumber}E${currentEpisodeNumber + 1}`
             currentSeries.seasons[seasonNumber - 1].episodes.push(newEpisode)
+            folderRef = `series/${currentSeries.seriesID}/season${seasonNumber}/episode${currentEpisodeNumber + 1}`
         }
 
 
         $.body.classList.add('uploading')
-        folderRef = `series/${currentSeries.seriesID}/season${seasonNumber}/episode${currentEpisodeNumber}`
         
         showUploadElems(videoQualities)
         showUploadElems(subtitles)
 
         try{
             const msg = await uploadData(videoQualities)
-            console.log(uploadedVideosCounter);
-            console.log(videoQualities.length);
             alert(msg)
 
             if(subtitles.length){
@@ -835,7 +833,7 @@ function renderEpisodes (seasonNumber){
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                    <svg onclick="removeEpisode(${index + 1}, ${seasonNumber})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                     </svg>
                 </div>
@@ -866,8 +864,10 @@ async function removeSeason(e, seasonNumber) {
     const shouldDelete = confirm(`Are you sure you want to delete season ${seasonNumber} of ${currentSeries.title} ?\nThis action is permanent.`)
 
     if(shouldDelete){
-        const btn = e.target.className === 'btn' ? e.target : e.target.parentElement
-        btn.classList.add('loading')
+        if(e){
+            const btn = e.target.className === 'btn' ? e.target : e.target.parentElement
+            btn.classList.add('loading')
+        }
         const seriesRef = doc(db, `series/${seriesID}`)
         const seasons = currentSeries.seasons.filter(season => season.seasonNumber != seasonNumber)
         const seasonRef = ref(storage, `series/${seriesID}/season${seasonNumber}`)
@@ -877,13 +877,21 @@ async function removeSeason(e, seasonNumber) {
             await setDoc(seriesRef, {seasons}, {merge : true})
             alert(`Season ${seasonNumber} of ${currentSeries.title} removed successfully`)
             currentSeries = allSeries.find(series => series.seriesID === seriesID)
-            renderSeasons(currentSeries.seasons)
-            episodesContainer.parentElement.classList.remove("show")
+
+            // check if any season left for this series
+            if(currentSeries.seasons.length){
+                renderSeasons(currentSeries.seasons)
+                episodesContainer.parentElement.classList.remove("show")
+            }else{
+                closeModal()
+            }
+
         } catch (error) {
             alert(`Oops, an error occurred while removing this season, please try again`)
             console.log(error);
         }
-        btn.classList.remove('loading')
+
+        if(e) btn.classList.remove('loading')
     }
 }
 
@@ -900,6 +908,43 @@ async function deleteFilesAndFolders(folderRef) {
     await Promise.all(subfolderPromises)
 }
 
+async function removeEpisode (episodeNumber, seasonNumber) {
+
+    // if this is the last episode of this season, remove the whole season
+    if(episodeNumber == 1){
+        removeSeason(undefined, seasonNumber)
+    }else{
+        const shouldDelete = confirm(`Are you sure you want to remove this episode ?`)
+    
+        if(shouldDelete){
+            // used JSON methods to avoid over-writing the main array (allSeries array)
+            let currentSeries = JSON.parse(JSON.stringify(allSeries.find(series => series.seriesID === seriesID)))
+            
+            // this references to the episode files in firebase cloud storage
+            const episodeFolderRef = ref(storage, `series/${seriesID}/season${seasonNumber}/episode${episodeNumber}`)
+            
+            // this references to current series data in fireStore database
+            const seriesRef = doc(db, `series/${seriesID}`)
+    
+            // remove the episode from currentSeries
+            currentSeries.seasons[seasonNumber - 1].episodes.splice(episodeNumber - 1, 1)
+            
+            try{
+                const filesList = await listAll(episodeFolderRef)
+                const filesPromises = filesList.items.map(item => deleteObject(item))
+                    
+                await Promise.all(filesPromises)
+                await setDoc(seriesRef, {seasons : currentSeries.seasons}, {merge : true})
+                alert(`Episode ${episodeNumber} of season${seasonNumber} deleted successfully.`)
+            
+                renderEpisodes(seasonNumber)
+            }catch(err){
+                alert('Oops, an error occurred while removing this episode.\nPlease try again.')
+                console.log(err);
+            }
+        }
+    }
+}
 
 // ---------- EVENT LISTENERS ---------- //
 
